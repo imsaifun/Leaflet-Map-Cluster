978250c335b5b437a03994b3c4a4a11b87c34479
Golam Mostafa <89465189+imsaifun@users.noreply.github.com>
2021-11-06 11:03:35 +0000

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,12 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-markercluster";
-import "leaflet/dist/leaflet.css";
-import "react-leaflet-markercluster/dist/styles.min.css";
+import dynamic from "next/dynamic"
+
+const MapCluster = dynamic(() => import("../Component/Map"), { ssr:false })
 
 export default function Home() {
   return (
-    <MapContainer
-            style={{ height: "90vh" }}
-            center={[51.0, 19.0]}
-            zoom={4}
-            maxZoom={18}
-            scrollWheelZoom={false}
-        >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-            <MarkerClusterGroup>
-                <Marker position={[49.8397, 24.0297]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-                <Marker position={[52.2297, 21.0122]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-                <Marker position={[51.5074, -0.0901]} />
-            </MarkerClusterGroup>
-        </MapContainer>
+    <>
+            <MapCluster/>
+        </>
   )
 }
 
